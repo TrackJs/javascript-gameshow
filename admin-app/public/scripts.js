45e4a03a6fccc1de8c3f47f944018bb3e2b61bdc
Todd Gardner <todd@g12solutions.com>
2022-12-15 13:30:45 -0600

    WIP admin app Co-authored-by: martine@dowden.us <martine@dowden.us>

--- admin-app/public/scripts.js
+++ admin-app/public/scripts.js
@@ -1,24 +1,94 @@
 'use strict';
 
 let user;
-let answers = answers
+let answers;
+let questions;
+let activeEventId;
+let activeQuestionId;
 
 (async () => {
   console.log('loaded');
 
   firebase.auth().onAuthStateChanged((_user) => {
-    console.log(_user)
+    console.log("user", _user);
     user = _user
+    const loginButton = document.querySelector('#login');
+    if (!user) {
+      loginButton.innerText = "Are you Cool?";
+    } else {
+      loginButton.innerText = `Hi ${user.displayName}!`;
+      handleListeners();
+    }
   })
 
-  const questionRef = firebase.database().ref('questions')
-  questionRef.on('value', snapsot => {
-    //  question => snapshot.val()
-    //  snapshot.id
-  })
+  async function handleListeners() {
+    console.log("starting listeners");
+    const form = document.querySelector("#selectQuestionForm");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const values = new FormData(form);
+      selectQuestion(values.get("question"));
+    })
 
+    const questionRef = firebase.database().ref('questions')
+    const activeEventRef = firebase.database().ref('activeEventId')
+
+    activeEventId = (await activeEventRef.get()).val()
+    // questionRef.get().then(snapshot => {
+    //   console.log(snapshot)
+    // })
+
+    document.querySelector("#eventId").innerHTML = `${activeEventId}`;
+
+    questionRef.on('value', snapshot => {
+      questions = snapshot.val();
+      const questionList = document.querySelector('#questionList');
+      questionList.innerHTML = "";
+      Object.keys(questions).forEach(questionId => {
+        const question = questions[questionId];
+
+        questionList.innerHTML += `
+        <li>
+          <label>
+            <input type="radio" value="${questionId}" name="question" />
+            <span>${question.questionText}</span>
+          </label>
+        </li>`;
+      });
+    })
+
+    const answersRef = firebase.database().ref(`/answers/${activeEventId}/${activeQuestionId}`)
+    answersRef.on('value', snapshot => {
+      answers = snapshot.val();
+
+      if (!answers) { return; }
+
+      const answerList = document.querySelector("#answerList");
+      answerList.innerHTML = "";
+      Object.keys(answers).forEach(a => {
+        const answer = questions[questionId];
+
+        answerList.innerHTML += `
+          <li>
+            <div>${answer.displayName}</div>
+            <div>${answer.answer}</div>
+          </li>`;
+      });
+    });
+
+  }
 
 })();
+
+async function selectQuestion(questionId) {
+  console.log("questionId", questionId);
+
+  const questionRef = firebase.database().ref(`questions/${questionId}`)
+  questionRef.update({ used: true })
+  const activeQuestionRef = firebase.database().ref('activeQuestion')
+  activeQuestionRef.set({...questions[questionId], eventId: activeEventId})
+  activeQuestionId = questionId
+}
 
 async function amITodd() {
   const provider = new firebase.auth.GoogleAuthProvider()
