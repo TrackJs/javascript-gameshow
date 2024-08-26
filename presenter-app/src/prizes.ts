efe2881297af31ab1afaad04f12fda2c0cde76c8
Todd Gardner <todd@g12solutions.com>
2024-08-26 17:48:10 -0500

    some prizes for copenhagen

--- presenter-app/src/prizes.ts
+++ presenter-app/src/prizes.ts
@@ -13,89 +13,57 @@ export const Prizes: Prize[] = [
   {
     name: "Hand Sanitizer",
     level: 0,
-    quantity: 4,
+    quantity: 6,
     imageUrl: "/assets/prizes/hand_sanitizer.png",
     sponsorName: "Request Metrics",
     sponsorImageUrl: "/assets/sponsors/request_metrics2.svg"
   },
   {
-    name: "Squeaky Sloth Keychain",
-    level: 0,
-    quantity: 5,
-    imageUrl: "/assets/prizes/sloth_keychain.png",
-    sponsorName: "Request Metrics",
-    sponsorImageUrl: "/assets/sponsors/request_metrics.svg"
-  },
-  {
-    name: "At Least I'm Wearing Pants",
+    name: "JavaScript Happpens TShirt",
     level: 1,
-    quantity: 5,
-    imageUrl: "/assets/prizes/jogger.png",
+    quantity: 6,
+    imageUrl: "/assets/prizes/tshirt.png",
     sponsorName: "TrackJS",
     sponsorImageUrl: "/assets/sponsors/trackjs.svg"
   },
+  // {
+  //   name: "Squeaky Sloth Keychain",
+  //   level: 0,
+  //   quantity: 5,
+  //   imageUrl: "/assets/prizes/sloth_keychain.png",
+  //   sponsorName: "Request Metrics",
+  //   sponsorImageUrl: "/assets/sponsors/request_metrics.svg"
+  // },
+  // {
+  //   name: "At Least I'm Wearing Pants",
+  //   level: 1,
+  //   quantity: 5,
+  //   imageUrl: "/assets/prizes/jogger.png",
+  //   sponsorName: "TrackJS",
+  //   sponsorImageUrl: "/assets/sponsors/trackjs.svg"
+  // },
   {
-    name: "JVC Waterproof Bluetooth Speaker",
+    name: "VIP Merch Pack",
     level: 2,
-    quantity: 3,
-    imageUrl: "/assets/prizes/jvc-speaker.png",
-    sponsorName: "Request Metrics",
-    sponsorImageUrl: "/assets/sponsors/request_metrics2.svg"
+    quantity: 1,
+    imageUrl: "/assets/prizes/TODO.png",
+    sponsorName: "Pieces for Developers",
+    sponsorImageUrl: "/assets/sponsors/pieces.png"
   },
   {
-    name: "￡100 Amazon GC",
+    name: "Anker Powerbank",
     level: 3,
-    quantity: 2,
-    imageUrl: "/assets/prizes/amazongc.png",
-    sponsorName: "Progress",
-    sponsorImageUrl: "/assets/sponsors/progress.jpg"
+    quantity: 1,
+    imageUrl: "/assets/prizes/anker_powerbank.png",
+    sponsorName: "Prosa",
+    sponsorImageUrl: "/assets/sponsors/prosa.png"
   },
   {
-    name: "Ember Mug",
-    level: 3,
-    quantity: 3,
-    imageUrl: "/assets/prizes/embermug.png",
-    sponsorName: "Syncfusion",
-    sponsorImageUrl: "/assets/sponsors/syncfusion.png"
-  },
-  {
-    name: "Nintendo Switch",
+    name: "Lego C3PO",
     level: 4,
     quantity: 1,
-    imageUrl: "/assets/prizes/nintendo_switch.png",
+    imageUrl: "/assets/prizes/lego_c3p0.png",
     sponsorName: "Text Control",
     sponsorImageUrl: "/assets/sponsors/text_control.svg"
-  },
-  {
-    name: "Apple Airpods",
-    level: 4,
-    quantity: 1,
-    imageUrl: "/assets/prizes/airpods.png",
-    sponsorName: "Highcharts",
-    sponsorImageUrl: "/assets/sponsors/highcharts.png"
-  },
-  {
-    name: "Sony Noise-Cancelling Headphones",
-    level: 4,
-    quantity: 3,
-    imageUrl: "/assets/prizes/sonyheadphones.png",
-    sponsorName: "TrackJS",
-    sponsorImageUrl: "/assets/sponsors/trackjs.svg"
-  },
-  {
-    name: "Surface Headphones 2",
-    level: 3, // 240gbp
-    quantity: 1,
-    imageUrl: "/assets/prizes/surfaceheadphones2.png",
-    sponsorName: "Microsoft",
-    sponsorImageUrl: "/assets/sponsors/microsoft.png"
-  },
-  {
-    name: "RODE NT-USB microphone",
-    level: 3, //130gbp
-    quantity: 1,
-    imageUrl: "/assets/prizes/rode_mic.png",
-    sponsorName: "Kontent.ai",
-    sponsorImageUrl: "/assets/sponsors/kontentai.png"
   }
 ];
