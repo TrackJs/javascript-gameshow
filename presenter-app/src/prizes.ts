de25d09ccde5c704ae5c172ed985870cf6f8f229
Todd Gardner <todd@g12solutions.com>
2024-08-28 15:00:14 +0200

    cph prizes

--- presenter-app/src/prizes.ts
+++ presenter-app/src/prizes.ts
@@ -46,9 +46,49 @@ export const Prizes: Prize[] = [
     name: "VIP Merch Pack",
     level: 2,
     quantity: 1,
-    imageUrl: "/assets/prizes/TODO.png",
+    imageUrl: "/assets/prizes/pieces-prize-pack.png",
     sponsorName: "Pieces for Developers",
     sponsorImageUrl: "/assets/sponsors/pieces.png"
+  },
+  {
+    name: "Lego Land Rover",
+    level: 2,
+    quantity: 1,
+    imageUrl: "/assets/prizes/lego-land-rover.png",
+    sponsorName: "NDC Conferences",
+    sponsorImageUrl: "/assets/sponsors/ndc.png"
+  },
+  {
+    name: "Lego Rose",
+    level: 2,
+    quantity: 1,
+    imageUrl: "/assets/prizes/lego-rose.png",
+    sponsorName: "NDC Conferences",
+    sponsorImageUrl: "/assets/sponsors/ndc.png"
+  },
+  {
+    name: "Dakota Powerbank",
+    level: 2,
+    quantity: 1,
+    imageUrl: "/assets/prizes/dakota-powerbank.png",
+    sponsorName: "TrackJS",
+    sponsorImageUrl: "/assets/sponsors/trackjs.svg"
+  },
+  {
+    name: "Philips Headphones",
+    level: 3,
+    quantity: 1,
+    imageUrl: "/assets/prizes/philips-headphones.png",
+    sponsorName: "TrackJS",
+    sponsorImageUrl: "/assets/sponsors/trackjs.svg"
+  },
+  {
+    name: "Lego Flowers",
+    level: 3,
+    quantity: 2,
+    imageUrl: "/assets/prizes/lego-flowers.png",
+    sponsorName: "NDC Conferences",
+    sponsorImageUrl: "/assets/sponsors/ndc.png"
   },
   {
     name: "Anker Powerbank",
@@ -65,5 +105,29 @@ export const Prizes: Prize[] = [
     imageUrl: "/assets/prizes/lego_c3p0.png",
     sponsorName: "Text Control",
     sponsorImageUrl: "/assets/sponsors/text_control.svg"
+  },
+  {
+    name: "Logitech Gaming Mouse",
+    level: 4,
+    quantity: 1,
+    imageUrl: "/assets/prizes/logitech-g502x.png",
+    sponsorName: "Request Metrics",
+    sponsorImageUrl: "/assets/sponsors/request_metrics2.svg"
+  },
+  {
+    name: "Lego Piranha Plant",
+    level: 4,
+    quantity: 1,
+    imageUrl: "/assets/prizes/lego-piranha.png",
+    sponsorName: "TrackJS",
+    sponsorImageUrl: "/assets/sponsors/trackjs.svg"
+  },
+  {
+    name: "Lego Chess Set",
+    level: 4,
+    quantity: 1,
+    imageUrl: "/assets/prizes/lego-chess-set.png",
+    sponsorName: "NDC Conferences",
+    sponsorImageUrl: "/assets/sponsors/ndc.png"
   }
 ];
